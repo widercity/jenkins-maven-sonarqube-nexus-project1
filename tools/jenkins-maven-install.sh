5cdee92c1641555a2fa6ea395d88871721141986
ubaka <duv75@yahoo.com>
2024-05-20 05:02:36 -0400

    my jenkins p

--- tools/jenkins-maven-install.sh
+++ tools/jenkins-maven-install.sh
@@ -27,7 +27,7 @@ mvn -v
 # Create ".m2" and download your "settings.xml" file into it to Authorize Maven
 ## Make sure to Update the RAW GITHUB Link to your "settings.xml" config
 mkdir /var/lib/jenkins/.m2
-wget https://github.com/widercity/jenkins-maven-sonarqube-nexus-project1.git -P /var/lib/jenkins/.m2/
+wget https://raw.githubusercontent.com/widercity/jenkins-maven-sonarqube-nexus-project1/main/tools/jenkins-maven-install.sh -P /var/lib/jenkins/.m2/
 chown -R jenkins:jenkins /var/lib/jenkins/.m2/
 chown -R jenkins:jenkins /var/lib/jenkins/.m2/settings.xml
 
